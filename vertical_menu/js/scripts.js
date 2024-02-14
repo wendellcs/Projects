e2ad4d5e8d967d95ca3bbb5792008cd6dd2c241e
Wendell <wendellcs131@gmail.com>
2024-02-14 14:13:05 -0300

    New scripts

--- vertical_menu/js/scripts.js
+++ vertical_menu/js/scripts.js
@@ -1,19 +1,32 @@
+const header = document.querySelector('#header')
+const logo = document.querySelector('.logo-container')
+
+const menu = document.querySelector('.menu')
 const menuControl = document.querySelector('.menu-control')
 const menuIcon = document.querySelector('.icon')
-const header = document.querySelector('#header')
 
-menuControl.addEventListener('click', () => {
+// Open and close the menu.
+menuControl.addEventListener('click', toggleElementClass)
 
-    if (!menuIcon.classList.contains('rotate')) {
-        menuIcon.classList.add('rotate')
-        header.classList.toggle('close')
-    } else {
-        menuIcon.classList.remove('rotate')
-        header.classList.toggle('close')
-    }
+const search = document.querySelector('.search-area')
+const searchIcon = document.querySelector('.search-icon')
+
+// Verify whether the menu is closed, if so, the user will be able to open it by clicking on the search icon.
+// The search icon can't close the menu.
+search.addEventListener('click', () => {
+    if (!header.classList.contains('close')) return
+    toggleElementClass()
 })
 
+const userContainer = document.querySelector('.container-user')
 
+// Toggle elements classes
+function toggleElementClass() {
+    header.classList.toggle('close')
 
-
-
+    menu.classList.toggle('small')
+    logo.classList.toggle('small')
+    search.classList.toggle('small')
+    menuIcon.classList.toggle('rotate')
+    userContainer.classList.toggle('small')
+}
