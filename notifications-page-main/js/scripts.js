de7488cd32a2d335674ccacce9c87e3d8af49698
Wendell <wendellcs131@gmail.com>
2024-02-17 01:01:41 -0300

    Finish css styles

--- notifications-page-main/js/scripts.js
+++ notifications-page-main/js/scripts.js
@@ -1,1 +1,9 @@
-
+const notifications = document.querySelectorAll('.notification')
+notifications.forEach(noti => noti.classList.add('unread'))
+notifications.forEach(noti => {
+    if (noti.classList.contains('unread')) {
+        noti.addEventListener('click', () => {
+            noti.classList.remove('unread')
+        })
+    }
+})
