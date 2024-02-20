5664643c730881b1317f69e0782f4ecd1bc6bb14
Wendell <wendellcs131@gmail.com>
2024-02-20 16:28:14 -0300

    Add scripts

--- notifications-page-main/js/scripts.js
+++ notifications-page-main/js/scripts.js
@@ -1,15 +1,28 @@
 const notifications = document.querySelectorAll('.notification')
+const notificationsCount = document.querySelector('.header-notifications-count')
+
 notifications.forEach(noti => noti.classList.add('unread'))
 notifications.forEach(noti => {
     if (noti.classList.contains('unread')) {
         noti.addEventListener('click', () => {
             noti.classList.remove('unread')
+            updateNotifications()
         })
     }
+    updateNotifications()
 })
 
 const btnMarkAllAsRead = document.querySelector('.header-button')
 
 btnMarkAllAsRead.addEventListener('click', () => {
     notifications.forEach(noti => noti.classList.remove('unread'))
+    updateNotifications()
 })
+
+function updateNotifications() {
+    let count = 0
+    notifications.forEach(noti => {
+        if (noti.classList.contains('unread')) count++
+    })
+    notificationsCount.textContent = count
+}
