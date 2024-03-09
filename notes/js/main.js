e7e66d8cca487a9434b7f0a70fcb2b3ccec6501d
Wendell <wendellcs131@gmail.com>
2024-03-09 00:32:52 -0300

    Add results scripts

--- notes/js/main.js
+++ notes/js/main.js
@@ -22,7 +22,7 @@ const qtd = document.querySelector('.qtd')
 
 const word = document.querySelector('.form-box-word')
 
-const fieldsPopup = document.querySelector('.container-invalid-data-popup')
+const popup = document.querySelector('.container-popup')
 const closePopupBtn = document.querySelector('.close-popup')
 form.addEventListener('submit', (e) => { e.preventDefault() })
 
@@ -32,12 +32,12 @@ sendBtn.addEventListener('click', (e) => {
         sendSearch(word.value)
         word.value = ''
     } else {
-        fieldsPopup.classList.remove('hidden')
+        popup.classList.remove('hidden')
     }
 })
 
 closePopupBtn.addEventListener('click', (e) => {
-    fieldsPopup.classList.add('hidden')
+    popup.classList.add('hidden')
 })
 
 // Render all the cards on the screen and get saved cards
@@ -58,7 +58,7 @@ function renderCards(saved) {
 
 // Create and return the cards HTML
 function createCard(card, index) {
-    const { word, titles, definitions } = card
+    const { word, titles } = card
 
     const cardDiv = document.createElement('div')
     cardDiv.classList.add('card')
@@ -68,13 +68,22 @@ function createCard(card, index) {
     deleteBtn.innerHTML = 'Delete'
     deleteBtn.setAttribute('data', index)
     deleteBtn.classList.add('btn', 'delete')
-
+    const viewBtn = document.createElement('button')
+    viewBtn.innerHTML = 'View'
+    viewBtn.classList.add('btn', 'view')
+    viewBtn.setAttribute('data', index)
 
     const _word = document.createElement('h2')
     _word.textContent = word
 
+    buttonsContainer.appendChild(deleteBtn)
+    buttonsContainer.appendChild(viewBtn)
+
+    cardDiv.appendChild(_word)
+
     const topics = document.createElement('select')
     topics.className = 'topics'
+    topics.setAttribute('data', index)
     titles.forEach(title => {
         const option = document.createElement('option')
         option.textContent = title
@@ -83,23 +92,20 @@ function createCard(card, index) {
         topics.appendChild(option)
     })
 
-    buttonsContainer.appendChild(deleteBtn)
-
-    cardDiv.appendChild(_word)
     cardDiv.appendChild(topics)
     cardDiv.appendChild(buttonsContainer)
-
-    console.log(cardDiv)
 
     return cardDiv
 }
 
 document.addEventListener("click", (e) => {
     // Close fields popup if clicked outside
-    if (!fieldsPopup.contains(e.target) && !e.target.classList.contains('formButton')) {
-        fieldsPopup.classList.add('hidden')
+    if (!popup.contains(e.target) && !e.target.classList.contains('formButton')) {
+        popup.classList.add('hidden')
     }
 })
+
+const containerResults = document.querySelector('.container-left-results')
 
 containerCards.addEventListener("click", (e) => {
     const index = e.target.getAttribute("data")
@@ -111,35 +117,74 @@ containerCards.addEventListener("click", (e) => {
         containerCards.removeChild(parent)
         renderCards()
     }
+
+    if (e.target.classList.contains('view')) {
+        const topic = document.querySelector(`.topics[data="${index}"]`)
+        const topicValue = topic.value
+
+        const cardInfos = cardList[index]
+        const definitions = createDefinitionList(cardInfos, topicValue)
+        const cardDefinition = createCardDefinition(definitions, topicValue)
+
+        containerResults.innerHTML = ''
+        containerResults.append(cardDefinition)
+    }
 })
 
+function createCardDefinition(definitions, topicValue) {
+    const resultBox = document.createElement('div')
+    resultBox.className = 'result-box'
 
-// Verify the height of the new card and if necessary, generate the popup further from the card's top
-function verifyCardHeight(parent) {
-    const elementHeigh = parent.offsetHeight
-    if (elementHeigh > 180) {
-        cardPopup.style.top = elementHeigh + 'px'
+    const resultTitle = document.createElement('h2')
+    resultTitle.className = 'result-box-title'
+    resultTitle.textContent = topicValue
+    resultBox.appendChild(resultTitle)
+
+    const containerDefinition = document.createElement('div')
+    containerDefinition.className = 'result-box-container-definition'
+
+    if (definitions.length > 1) {
+        for (let i = 0; i < definitions.length; i++) {
+            for (let j = 0; j < definitions[i].length; j++) {
+                let resultDefinition = document.createElement('p')
+                resultDefinition.className = 'definition'
+                resultDefinition.textContent = definitions[i][j].definition
+                containerDefinition.appendChild(resultDefinition)
+            }
+        }
     } else {
-        cardPopup.style.top = '180px'
+        definitions[0].forEach(defin => {
+            let resultDefinition = document.createElement('p')
+            resultDefinition.className = 'definition'
+            resultDefinition.textContent = defin.definition
+            containerDefinition.appendChild(resultDefinition)
+        })
     }
+
+    resultBox.appendChild(containerDefinition)
+
+    return resultBox
 }
 
-// Preventing default popup-form behavior 
+function createDefinitionList(cardInfos, topicValue) {
+    const definitionsList = []
+    const { definitions, titles } = cardInfos
+
+    for (let i = 0; i < titles.length; i++) {
+        if (titles[i] === topicValue) {
+            definitionsList.push(definitions[i])
+        }
+    }
+
+    const defin = [...definitionsList]
+
+    return defin
+}
+
 // Saves all the notes in the user's browser
 function updateAndSaveCards(list) {
     localStorage.clear()
     localStorage.setItem('cards', JSON.stringify(list))
-}
-
-function returnCardIndex(element) {
-    const documentCards = document.getElementsByClassName('card')
-
-    for (let i = 0; i < documentCards.length; i++) {
-        if (documentCards[i] == element) {
-            return i
-        }
-    }
-    return -1
 }
 
 // Get every note which has been saved in the browser
